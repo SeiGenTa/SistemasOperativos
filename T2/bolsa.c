b9061deae6ad7ab93f0b89d592ab95765de43066
AlMartinez01 <almartinez@ug.uchile.cl>
2022-09-26 22:16:40 -0300

    a

--- T2/bolsa.c
+++ T2/bolsa.c
@@ -9,39 +9,41 @@
 int *isSold; //if is 1, so was sold
 
 //Info of seller
-char **nameSeller;
-int *priceSeller = NULL; //Cond initial is Null
+char *nameSeller;
+int priceSeller = 0; //Cond initial is 0
 
 //Info of buyer
-char **nameBuyer;
+char **nameBuyer; //Innecesario
 
 //Cond and Mutex of pthread
 static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 
 int vendo(int precio, char *vendedor, char *comprador) {
-    int* myPrice = malloc(sizeof(int));
-    *myPrice = precio;//init: our price
     int* myIsSold = malloc(sizeof(int));
     *myIsSold = 0; //init: false
     char** myBuyer = malloc(sizeof(char*));
     *myBuyer = comprador;//init: this will modify our buyer
-    char** myseller = malloc(sizeof(char*));
-    *myseller = vendedor;//init: our name
 
     pthread_mutex_lock(&m);
-    if(priceSeller == NULL){//if not exist a seller, so we change the global data with the own data
+    if(priceSeller == 0){//if not exist a seller, so we change the global data with the own data
         isSold = myIsSold;
-        priceSeller = myPrice;
-        nameSeller = myseller;
+        priceSeller = precio;
+        nameSeller = vendedor;
         nameBuyer = myBuyer;
     }
-    else if(*priceSeller > *myPrice){//if we have better price than the actually seller, so we change the global data with the own data
+    else if(priceSeller > precio){//if we have better price than the actually seller, so we change the global data with the own data
         isSold = myIsSold;
-        priceSeller = myPrice;
-        nameSeller = myseller;
+        priceSeller = precio;
+        nameSeller = vendedor;
         nameBuyer = myBuyer;
         pthread_cond_broadcast(&c);// and we wake up the previus seller
+    }
+    else{
+        free(myBuyer);
+        free(myIsSold);
+        pthread_mutex_unlock(&m);
+        return 0;
     }
 
     pthread_cond_wait(&c,&m);
@@ -51,10 +53,8 @@ int vendo(int precio, char *vendedor, char *comprador) {
         t = 1; //now we return 1 
     }
     //we free the memory
-    free(myPrice);
     free(myBuyer);
     free(myIsSold);
-    free(myseller);
     
     return t;
 }
@@ -62,13 +62,13 @@ int vendo(int precio, char *vendedor, char *comprador) {
 
 int compro(char *comprador, char *vendedor) {
     pthread_mutex_lock(&m);
-    if (priceSeller == NULL){
+    if (priceSeller == 0){
         pthread_mutex_unlock(&m);
         return 0;
     }
     //we obtain the seller´s data
-    strcpy(vendedor,*nameSeller);
-    int price = *priceSeller;
+    strcpy(vendedor,nameSeller);
+    int price = priceSeller;
     *isSold = 1;
     //and change the buyer's data with the own
     strcpy(*nameBuyer,comprador);//now the seller has the seller's name
@@ -77,7 +77,7 @@ int compro(char *comprador, char *vendedor) {
     isSold = NULL;
     nameBuyer = NULL;
     nameSeller = NULL;
-    priceSeller = NULL;
+    priceSeller = 0;
 
     pthread_cond_broadcast(&c);
     pthread_mutex_unlock(&m);
